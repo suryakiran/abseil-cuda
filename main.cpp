0947506a4d6da2c472a248da726e44be193933f0
Surya <suryakiran.gullapallI@gmail.com>
2022-02-03 16:18:31 +0530

    Initial version

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <absl/container/flat_hash_set.h>
+
+using namespace std;
+
+int main(void) {
+  return 0;
+}
---
+++ test.cu
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <absl/container/flat_hash_set.h>
+
+void test(){
+  std::cout << "test cuda" << std::endl;
+}
